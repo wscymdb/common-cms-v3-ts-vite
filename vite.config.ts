372e796e750282aa33642b5de49bd7204018ea7e
“wscymdb” <1724215773@qq.com>
2023-06-08 17:07:04 +0800

    chore(all): 添加@路径语法糖

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { fileURLToPath, URL } from 'node:url'
+
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
@@ -18,5 +20,10 @@ export default defineConfig({
   ],
   server: {
     open: true
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   }
 })
